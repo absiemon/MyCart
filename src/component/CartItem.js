4ace199696b6c3c6ae9611e1267ef41cd52d3943
Abhay <abc@gmail.com>
2022-05-07 10:36:57 +0530

    Added state to the CartItem component

--- src/component/CartItem.js
+++ src/component/CartItem.js
@@ -10,20 +10,36 @@ const style = {
     }
 };
 export default class CartItem extends Component {
+    // to declaring state using constructor
+    constructor(){
+        super();
+        // ths.state will be a obejct
+        this.state={
+            title:"Phone",
+            price: 999,
+            Qty: 1,
+            img: '/'
 
+        }
+    }
   render() {
+// Object destructuring
+    const{title, price, Qty} = this.state;
     return (
       <div className="cart-item ">
           <div className="left-block">
               <img style={style.image}/>
           </div>
           <div className="right-block">
-              <div style={{fontSize: 25}}> Phone</div>
-              <div style={{color: '#777'}}> Rs- 999</div>
-              <div style={{color: '#777'}}>Qty: 1</div>
+              <div style={{fontSize: 25}}> {title}</div>
+              <div style={{color: '#777'}}> Rs- {price}</div>
+              <div style={{color: '#777'}}>Qty: {Qty}</div>
               <div className="cart-item-action">
 
-                  {/* buttons */}
+                <img alt="increase" className="action-icon" src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1651898950~hmac=1648bf0a05f56dc28201561e8db5b077"/>
+                <img alt="decrease" className="action-icon" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"/>
+                <img alt="delete" className="action-icon" src="https://cdn-icons.flaticon.com/png/512/484/premium/484611.png?token=exp=1651899123~hmac=2ef1e1f866dcd7398c91e876eb3cb334"/>
+
               </div>
 
           </div>
